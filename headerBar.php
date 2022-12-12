e7068403f421c34bb1a2022ea5f2d0fc823b0090
Parth Dhaduk <parthdhaduk2456@gmail.com>
2022-12-12 08:20:27 +0530

    Header Bar Change
    Add New file headerBar.php

---
+++ headerBar.php
@@ -0,0 +1,21 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="home.php">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="display.php">Records</a>
+                </li>
+            </ul>
+        </div>
+        <div>
+            <a href="logout.php" class="btn btn-danger" onclick="return confirm('Are you sure this account is logout.');">Logout</a>
+        </div>
+    </div>
+</nav>
